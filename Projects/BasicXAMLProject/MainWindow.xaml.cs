37338ac850e733534c3e5bf75626228e3e10d6f3
David Bawden <dbawden@outlook.com>
2018-07-19 19:12:23 +0100

    Added small border with copy/paste buttons. Hooked up command bindings to perform code execution when they are clicked.

--- Projects/BasicXAMLProject/MainWindow.xaml.cs
+++ Projects/BasicXAMLProject/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,23 @@ namespace BasicXAMLProject
         public MainWindow()
         {
             InitializeComponent();
+
+            CommandBinding copyCommand = new CommandBinding(ApplicationCommands.Copy);
+            CommandBinding pasteCommand = new CommandBinding(ApplicationCommands.Paste);
+            this.CommandBindings.Add(copyCommand);
+            this.CommandBindings.Add(pasteCommand);
+            copyCommand.Executed += new ExecutedRoutedEventHandler(copyCommand_Executed);
+            pasteCommand.Executed += new ExecutedRoutedEventHandler(pasteCommand_Executed);
+        }
+
+        private void pasteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Debug.WriteLine("Paste exceuted");
+        }
+
+        private void copyCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Debug.WriteLine("Copy exceuted");
         }
     }
 }
